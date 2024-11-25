419f44fe5b989b2c98c96587a2d9786472fc51f1
clarapereiraa <clasipereira@gmail.com>
2024-11-25 14:35:00 -0300

    data de nascimento

--- sistemavio04-1/front/appEvento.js
+++ sistemavio04-1/front/appEvento.js
@@ -2,6 +2,17 @@ document
 .getElementById("form-selecao-data")
 .addEventListener("submit", testeCalendario);
 
-function testeCalendario(evento){
-    
+function testeCalendario(event){
+    //previne o comportamento padrao do formulario, ou seja, impede que ele seja enviado e recarregue a pagina
+    event.preventDefault();
+
+    const data_recebida = document.getElementById("data").value;
+
+    if(data_recebida){
+     //console.log("Data recebida: ", data_recebida);
+    alert("A data selecionada é: " + data_recebida);
+}
+else{
+    alert("Por favor, selecionar uma data!")
+}
 }
